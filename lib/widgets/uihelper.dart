d27cd0d623fdc21ffc419c59ca22f5d012bd7e23
rajukarki467 <rajukarki467@gmail.com>
2025-09-17 20:37:25 +0545

    doenload assets file and  remove default code from main.dart

--- lib/widgets/uihelper.dart
+++ lib/widgets/uihelper.dart
@@ -1,3 +1,39 @@
+import 'package:flutter/material.dart';
+
 class UiHelper {
-  static Custom
+  static CustomButton({
+    required VoidCallback callback,
+    required String buttonname,
+  }) {
+    return SizedBox(
+      height: 35,
+      width: 300,
+      child: ElevatedButton(
+        onPressed: () {
+          callback();
+        },
+        child: Text(
+          buttonname,
+          style: TextStyle(fontSize: 14, color: Colors.white),
+        ),
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent),
+      ),
+    );
+  }
+
+  static CustomText({
+    required String text,
+    required double height,
+    Color? color,
+    FontWeight? fontWeight,
+  }) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: height,
+        color: color ?? Color(0xff5e5e5e),
+        fontWeight: fontWeight,
+      ),
+    );
+  }
 }
