1ffcaae94090c42968afa007943291a6524bac17
rajukarki467 <rajukarki467@gmail.com>
2025-09-18 17:02:50 +0545

    complete status screen

--- lib/screen/home/status/status_screen.dart
+++ lib/screen/home/status/status_screen.dart
@@ -1,10 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:whatapp_clone/widgets/uihelper.dart';
 
 class StatusScreen extends StatelessWidget {
   const StatusScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("camera Screen")));
+    var statusContent = [
+      {
+        "images":
+            "https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "name": "Pawan",
+        "statustime": "25 min ago",
+      },
+      {
+        "images":
+            "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
+        "name": "Riya",
+        "statustime": " 2 hour ago",
+      },
+      {
+        "images":
+            "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde",
+        "name": "Aarav",
+        "statustime": "15 min ago",
+      },
+    ];
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 30),
+          Row(
+            children: [
+              SizedBox(width: 20),
+              UiHelper.CustomText(text: "Status", height: 20),
+            ],
+          ),
+          SizedBox(height: 10),
+          ListTile(
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundImage: NetworkImage(
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUspugOXub65sbxVHOEaD-JEKC8NNWgkWhlg&s",
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: CircleAvatar(
+                      radius: 10,
+                      backgroundColor: Color(0xff008069),
+                      child: Icon(
+                        Icons.add,
+                        color: Color(0xffffffff),
+                        size: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            title: UiHelper.CustomText(text: "My Status", height: 20),
+            subtitle: UiHelper.CustomText(
+              text: "Tap to add status update",
+              height: 15,
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                UiHelper.CustomText(text: "Recent updates", height: 15),
+                Icon(Icons.arrow_drop_down, color: Color(0xff5e5e5e)),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                      statusContent[index]['images'].toString(),
+                    ),
+                  ),
+                  title: UiHelper.CustomText(
+                    text: statusContent[index]['name'].toString(),
+                    height: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  subtitle: UiHelper.CustomText(
+                    text: statusContent[index]['statustime'].toString(),
+                    height: 14,
+                  ),
+                );
+              },
+              itemCount: statusContent.length,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
